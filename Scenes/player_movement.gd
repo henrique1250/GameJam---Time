d50da470de9cdfcefc00e8bf49697bfe7974c703
Joao-Victor-AVM <almeidamelo2012@gmail.com>
2026-08-16 10:35:30 -0300

    Controles e Fisica do Chão

--- Scenes/player_movement.gd
+++ Scenes/player_movement.gd
@@ -13,7 +13,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
