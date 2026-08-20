f9192fc2e6ab3ae4573741646bde624f19ca80be
Vini <bviniciusm@hotmail.com>
2026-08-20 11:24:40 -0300

    Revert "Merge NPC + Outline (Shader)"
    This reverts commit 9cf63455d5e5bdca931bf487b92fc37995cbb340, reversing
    changes made to fb70d41bac3ce2ffd50845c8ce95b1cfe7a63c0b.

--- Scenes/carrousel_container.gd
+++ Scenes/carrousel_container.gd
@@ -2,8 +2,6 @@
 extends Node2D
 class_name CarroselContainer
 
-signal song_selected(index: int)
-
 @export var spacing: float = 20.0
 
 @export var wraparound_enabled:bool = false
@@ -19,9 +17,6 @@ signal song_selected(index: int)
 @export var follow_button_focus: bool = false
 
 @export var position_offset_node: Control = null
-
-
-
 
 func _enter_tree() -> void:
 	add_to_group("jukebox_carrossel") #criando um grupo para utilizar no script da jukebox
@@ -91,7 +86,3 @@ func _on_left_pressed() -> void:
 
 func _on_right_pressed() -> void:
 	_right()
-
-
-func _on_button_pressed() -> void:
-	song_selected.emit(select_index)
