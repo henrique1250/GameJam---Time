3cbe845d3dc59c498963d4e17d6ef338f5eca049
Vini <bviniciusm@hotmail.com>
2026-08-21 09:09:32 -0300

    musica-aleatoria-test

--- Scenes/jukebox.gd
+++ Scenes/jukebox.gd
@@ -5,6 +5,9 @@ extends Area3D
 @onready var camera: Camera3D =  get_tree().get_first_node_in_group("main_camera")
 @onready var carrossel: Node2D = get_tree().get_first_node_in_group("jukebox_carrossel")
 
+@export var musicas: Array[AudioStream] = []
+@export var audio_player: AudioStreamPlayer
+
 func _ready() -> void:
 	input_event.connect(_on_input_event)
 
@@ -14,4 +17,10 @@ func _on_input_event(_camera, event: InputEvent, _pos, _normal, _idx) -> void:
 		print(carrossel)
 		if carrossel:
 			carrossel.visible = true
+			
+func _on_carrosel_container_song_selected(index: int) -> void:
+	if index >= 0 and index < musicas.size():
+		audio_player.stream = musicas[index]
+		audio_player.play()
+
 			
