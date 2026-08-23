5e877a46c5ea024079869b899e2834b83930721b
Vini <bviniciusm@hotmail.com>
2026-08-23 01:14:05 -0300

    npc-update

---
+++ Scripts/spawn_npc.gd
@@ -0,0 +1,63 @@
+extends Node3D
+
+@export var npc_scene: PackedScene
+@export var spawn_on_start: bool = true
+@export var spawn_interval:float = 5.0
+@export var max_npc = 10
+@export var spawned_count:int = 1
+@onready var timer: Timer = $Timer
+@export var occupied_radius: float = 1.0  # raio pra considerar um ponto "ocupado"
+
+@onready var spawn_points: Array[Marker3D] = []
+
+func _ready() -> void:
+	
+	for child in get_children():
+		if child is Marker3D:
+			spawn_points.append(child)
+	
+	timer.wait_time = spawn_interval
+	timer.timeout.connect(_on_spawn_timer_timeout)
+	timer.start()
+	
+	if spawn_on_start:
+		spawn_npc()
+		
+func _on_spawn_timer_timeout() -> void:
+	if spawned_count >= max_npc:
+		timer.stop()
+		return
+	
+	spawn_npc()
+	spawned_count += 1
+			
+
+func spawn_npc() -> void:
+	if not npc_scene:
+		push_warning("Nenhuma cena de NPC atribuída no Spawner!")
+		return
+	
+	var npc_instance = npc_scene.instantiate() as CharacterBody3D
+	
+
+	npc_instance.global_transform = global_transform
+
+	get_parent().add_child.call_deferred(npc_instance)
+	
+func _get_free_spawn_point() -> Marker3D:
+	# embaralha pra não sempre tentar na mesma ordem
+	var shuffled := spawn_points.duplicate()
+	shuffled.shuffle()
+
+	for point in shuffled:
+		if not _is_point_occupied(point):
+			return point
+
+	return null
+
+
+func _is_point_occupied(point: Marker3D) -> bool:
+	for npc in get_tree().get_nodes_in_group("npc"):
+		if npc.global_position.distance_to(point.global_position) < occupied_radius:
+			return true
+	return false
