910add42c30aeabae4ce65732b10bdf24ddbbef9
Joao-Victor-AVM <almeidamelo2012@gmail.com>
2026-09-03 15:26:37 -0300

    Transicao Colocada Segunda Vez

--- Scenes/menu_main.gd
+++ Scenes/menu_main.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var botao_iniciar = $Panel/CenterContainer/VBoxContainer/Jogar
 @onready var botao_sair = $Panel/CenterContainer/VBoxContainer/Sair
+@onready var transition_control = $TransicaoControlFade/TransitionControl
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,7 +10,9 @@ func _ready() -> void:
 	botao_sair.pressed.connect(_on_botao_sair_pressed)
 	
 func _on_botao_iniciar_pressed():
-	get_tree().change_scene_to_file("res://Scenes/mainscene.tscn")
+	transition_control.fade_to_scene("res://Scenes/trancisao_intro.tscn")
+	#get_tree().change_scene_to_file("res://Scenes/trancisao_intro.tscn")
+	#get_tree().change_scene_to_file("res://Scenes/mainscene.tscn")
 
 func _on_botao_sair_pressed():
 	get_tree().quit()
